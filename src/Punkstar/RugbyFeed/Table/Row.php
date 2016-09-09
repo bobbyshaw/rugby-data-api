3ca9efeefbb0aea5f39314c9c0c4a1ade11ac8bc
Nick Jones <nick@nicksays.co.uk>
2016-09-09 21:48:31 +0100

    Add league tables for Aviva and Pro12 leagues

---
+++ src/Punkstar/RugbyFeed/Table/Row.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Punkstar\RugbyFeed\Table;
+
+class Row
+{
+    public $position;
+    public $team;
+    public $played;
+    public $won;
+    public $drawn;
+    public $lost;
+    public $for;
+    public $against;
+    public $bonus_points;
+    public $points;
+}
