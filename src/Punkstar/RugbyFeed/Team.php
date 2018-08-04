f56e5127c3e5fc76582b23df16c72ac0022cc49f
Tom Robertshaw <me@tomrobertshaw.net>
2018-08-04 09:27:49 +0100

    Remove commented out code.

--- src/Punkstar/RugbyFeed/Team.php
+++ src/Punkstar/RugbyFeed/Team.php
@@ -4,23 +4,6 @@ namespace Punkstar\RugbyFeed;
 
 class Team
 {
-//    /**
-//     * @param $name
-//     * @return Team
-//     */
-//    public static function build($name)
-//    {
-//        foreach (self::$team_map as $team_key => $team_data) {
-//            $team_aliases = array_map('strtolower', $team_data['aliases']);
-//
-//            if (in_array(strtolower($name), $team_aliases)) {
-//                return new self($team_key);
-//            }
-//        }
-//
-//        return new self($name);
-//    }
-
     public function __construct($data)
     {
         $this->data = $data;
@@ -30,7 +13,7 @@ class Team
     {
         return $this->data['url'] ?? str_replace(' ', '_', strtolower($this->getName()));
     }
-    
+
     /**
      * @param $searchString
      * @return bool
@@ -40,10 +23,10 @@ class Team
         $aliases = array_map('strtolower', $this->data['alias'] ?? []);
         $aliases[] = strtolower($this->getName());
         $aliases[] = strtolower($this->getUrlKey());
-        
+
         return in_array(strtolower($searchString), $aliases);
     }
-    
+
     public function getName()
     {
         return $this->data['name'] ?? 'Unknown Name';
@@ -64,27 +47,4 @@ class Team
     {
         return $this->data['conference'] ?? '';
     }
-//
-//    /**
-//     * @param $alias
-//     * @return bool
-//     */
-//    public function isTeamAlias($alias)
-//    {
-//        return in_array(strtolower($alias), $this->getTeamAliases(), true);
-//    }
-//
-//    /**
-//     * Get lowercased valid team aliases.
-//     *
-//     * @return array
-//     */
-//    public function getTeamAliases()
-//    {
-//        $aliases = static::$team_map[$this->key]['aliases'];
-//        $aliases[] = $this->key;
-//        $aliases[] = $this->name;
-//
-//        return array_unique(array_map('strtolower', $aliases));
-//    }
 }
